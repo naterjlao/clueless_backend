2bea6dd9c91793bf0d2f8398ed8418d529c1443a
Nate Lao <lao.nathan@yahoo.com>
2020-03-31 00:34:46 -0400

    pushing comments and data digest

--- main.py
+++ main.py
@@ -13,15 +13,39 @@ log = open(LOG,'w')
 log.write('Skeletal Demo Backend Log')
 log.flush()
 
+# Create variables that is stored at runtime for this process
+x = 200
+y = 200
+SPEED = 5
+
 # Spinup a listener, this will be killed when the Serverside application is killed
 while True:
-	# Wait for input from the Serverside application
-	data = input()
-	
-	# Write out to log file what occurred
-	log.write("recieved %s" % data)
-	log.write
-	
-	# Send a confirmation to the Serverside that the message was recieved
-	print("message recieved", flush=True)
-	
+    # Wait for input from the Serverside application
+    data = input()
+    data = data.strip()
+    
+    # Determine what to do with that data
+    switch (data) {
+        case "left":
+            x = x - SPEED
+            break
+        case "right":
+            x = x + SPEED
+            break
+        case "up":
+            y = y - SPEED
+            break
+        case "down":
+            y = y + SPEED
+            break
+    }
+    
+    # Write out to log file what occurred
+    log.write("recieved %s" % data)
+    log.write("updating x -> $d" % x)
+    log.write("updating y -> $d" % y)
+    log.flush()
+    
+    # Send data back to the Serverside -- MUST BE JSON
+    print(("{x:%d,y:%d}" % x,y), flush=True)
+    

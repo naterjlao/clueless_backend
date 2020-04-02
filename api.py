0ac6b287f385aa3a68d6974365f7608029ffc33e
T <32947002+cheersmate@users.noreply.github.com>
2020-04-01 18:50:06 -0700

    Update api.py

--- api.py
+++ api.py
@@ -3,40 +3,16 @@
 
 class Players(restful.Resource):
 
-	def registerNewPlayer(self):
-
-		return 
-
-	def getPlayersList(self):
-
-		return 
-
 
 class Player(restful.Resource):
-
-	def getPlayerInfo(self):
-
-		return 
-
-	def assignSuspectInfo(self):
-
-		return
 
 
 
 class Games(restful.Resource):
 
-	def registerNewGame(self):
-
 
 class Game(restful.Resource):
-
-
-	def getGameState(self):
-
-
-	def deleteGame():
-
+	
 
 class movePlayer(restful.Resource):
 
@@ -48,7 +24,6 @@ class respondSuggestion():
 
 
 class makeAccusation():
-
 
 
 class endTurn():

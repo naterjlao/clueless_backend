ba88b57485c2697b26761be5986d475bc84995c7
cheersmate <ptpowell888@gmail.com>
2020-04-01 18:40:49 -0700

    Functional GameObjectModel and Backend Logic

--- Entity.py
+++ Entity.py
@@ -1,96 +1,403 @@
 
-class GameBuilder():
 
-	def buildGameCardDeck():
+# Set up Constants for Characters, Rooms, Names and Turns Variables
 
-	def buildGamePlayers():
+SUSPECT = 'Suspect'
+MUSTARD = 'Colonel Mustard'
+SCARLET = 'Miss Scarlet'
+PLUM = 'Professor Plum'
+GREEN = 'Mr Green'
+WHITE = 'Mrs White'
+PEACOCK = 'Mrs Peacock'
+
+SUSPECTS = [MUSTARD, SCARLET, PLUM, GREEN, WHITE, PEACOCK]
+
+WEAPON = 'Weapon'
+ROPE = 'Rope'
+LEAD_PIPE = 'Lead Pipe'
+KNIFE = 'Knife'
+WRENCH = 'Wrench'
+CANDLESTICK = 'Candlestick'
+REVOLVER = 'Revolver'
+
+WEAPONS = [ROPE,LEAD_PIPE,KNIFE,WRENCH,CANDLESTICK,REVOLVER]
+
+ROOM = 'Room'
+KITCHEN = 'Kitchen'
+BALLROOM = 'Ballroom'
+CONSERVATORY = 'Conservatory'
+BILLIARD_ROOM = 'Billiard Room'
+LIBRARY = 'Library'
+STUDY = 'Study'
+HALL = 'Hall'
+LOUNGE = 'Lounge'
+DINING_ROOM = 'Dining Room'
+
+ROOMS = [KITCHEN,BALLROOM,CONSERVATORY,BILLIARD_ROOM,LIBRARY,STUDY,HALL,LOUNGE,DINING_ROOM]
+
+STUDY_LIBRARY = 'study-library Passage'
+STUDY_HALL = 'study-hall Passage'
+HALL_BILLIARD = 'hall-billiard Passage'
+HALL_LOUNGE = 'hall-lounge Passage'
+LOUNGE_DINING = 'lounge-dining Passage'
+LIBRARY_CONSERVATORY = 'library-conservatory Passage'
+LIBRARY_BILLIARD = 'library-billiard Passage'
+BILLIARD_BALLROOM = 'billiard-ballroom Passage'
+BILLIARD_DINING = 'billiard-dining Passage'
+DINING_KITCHEN = 'dining-kitchen Passage'
+CONSERVATORY_BALLROOM = 'conservatory-ballroom Passage'
+BALLROOM_KITCHEN = "ballroom-kitchen Passage"
+
+Passages = [STUDY_LIBRARY,STUDY_HALL,
+			HALL_BILLIARD,HALL_LOUNGE,
+			LOUNGE_DINING,
+			LIBRARY_CONSERVATORY, LIBRARY_BILLIARD,
+			BILLIARD_BALLROOM, BILLIARD_DINING,
+			DINING_KITCHEN,
+			CONSERVATORY_BALLROOM,
+			BALLROOM_KITCHEN
+			]
+
+AWAITING_MOVE = "Awaiting Movee"
+AWAITING_SUGGESTION = "Awaiting Suggestion"
+AWAITING_SUGGESTION_RESPONSE = "Awaiting Response"
+AWAITING_ACCUSATION_OR_END_TURN = "Awaiting Accusation or End of Turn"
+
+
+# Player Class is for each Player in Game
+
+class Player(object):
+
+	def __init__(self, user, suspect = None, card_hand = None, card_seen = None):
+
+		self.user = user
+
+		if suspect:
+			self.suspect = suspect
+		else:
+			self.suspect = None
+		if card_hand:
+			self.card_hand = card_hand
+		else:
+			self.card_hand = list()
+		if card_seen:
+			self.card_seen = card_seen
+		else:
+			self.card_seen = list()
+
+
+	def format(self):
+
+		return{
+			"user": self.user,
+			"suspect": self.suspect,
+			"card_hand": [game_card.format() for game_card in self.card_hand],
+			"card_seen:": [game_card.format() for game_card in self.card_seen]
+		}
+
+
+# Card Class for each Card Item (Weapon, Character, Room)
+class Card(object):
+
+	def __init__(self, item, item_type):
+		self.item = item
+		self.type = item_type
+
+	def format(self):
+		return {
+			"item": self.item,
+			"item_type": self.type,
+		}
+
+# Suggestion Class for both Suggestions and Accusations
+class Suggestion(object):
+
+	def __init__(self, suspect, weapon, room):
+
+		self.weapon = weapon
+		self.suspect = suspect
+		self.room = room
+
+	def format(self):
+
+		return{
+			"suspect": self.suspect,
+			"weapon": self.weapon,
+			"room": self.room
+		}
+
+# BoardObject is interface for Rooms, Passages, HomeSpaces
+
+class BoardObject(object):
+
+	def __init__(self, name, connected, suspects = None):
+
+		self.name = name
+		self.connected = connected
+		if suspects:
+			self.suspects = suspects
+		else:
+			self.suspects = list()
+
+
+	#Availability Rules differ by BoardObject Type
+	def available(self):
+		pass
+
+	def format(self):
+
+		return {
+			"name": self.name,
+			"connected": self.connected,
+			"suspects": self.suspects
+		}
+
+
+#Room Class for Room BoardObject
+class Room(BoardObject):
+
+	def __init__(self, name, connected, suspects = None, weapons = None):
+
+		super(Room, self).__init__(name, connected, suspects)
+
+		if weapons:
+			self.weapons = weapons
+		else:
+			self.weapons = list()
+
+	# Multiple Players can be in Room
+	def available(self):
+		pass
+
+	def format(self):
+
+		return {
+				"name": self.name,
+				"connected": self.connected,
+				"suspects": self.suspects,
+				"weapons": self.weapons
+		}
 
 
 
-class Character(object): 
+class Passage(BoardObject):
 
-	CHARACTER_LIST = [
-		Ms_Scarlet,
-		Mr_Green,
-		Mrs_White,
-		Mrs_Peacock,
-		Prof_Plum,
-		Colonel Mustard,
+	def __init__(self, name, connected, suspects = None):
+
+		super(Passage, self).__init__(name, connected, suspects)
+
+	#Only One Player in Passage at a Time
+	def available(self):
+		return not self.suspects
+
+
+class HomeSpace(BoardObject):
+
+	def __init__(self, name, connected, suspects = None):
+		super(HomeSpace, self).__init__(name, connected, suspects)
+
+	#Return False since this is not an available Space after first Move
+	def available(self):
+		return False
+
+# GameModel Class formats a GameModel Dictionary to be used in GameModelBuilder class
+
+class GameModel(object):
+
+	def __init__(self, game, players, player_messages = None,
+				 turn_list = None, current_player = None, turn_status = None,
+				 current_suggestion = None, suggestion_response = None,
+				 case_file = None, game_active = True, game_winner = None,
+				 game_board = None):
+
+		self.game = game
+
+		self.players = players
+
+		if player_messages:
+			self.player_messages = player_messages
+		else:
+			self.player_messages = list()
+
+		if turn_list:
+			self.turn_list = turn_list
+		else:
+			self.turn_list = list(players)
+
+		if current_player:
+			self.current_player = current_player
+		else:
+			self.current_player = self.turn_list[0]
+
+		if turn_status:
+			self.turn_status = turn_status
+		else:
+			self.turn_status = AWAITING_MOVE
+
+		self.current_suggestion = current_suggestion
+
+		self.suggestion_response = suggestion_response
+
+		if case_file:
+			self.case_file = case_file
+		else:
+			self.case_file = list()
+
+		self.game_active = game_active
+
+		self.game_winner = game_winner
+
+		if game_board:
+			self.game_board = game_board
+
+		else:
+			self.game_board = {
+
+				KITCHEN: Room(name=KITCHEN, connected=[ DINING_KITCHEN, BALLROOM_KITCHEN, STUDY],weapons=[ROPE]),
+
+				BALLROOM: Room( name=BALLROOM, connected=[ BALLROOM_KITCHEN, CONSERVATORY_BALLROOM,BILLIARD_BALLROOM],weapons=[LEAD_PIPE]),
+
+				CONSERVATORY: Room(name=CONSERVATORY, connected=[CONSERVATORY_BALLROOM, LIBRARY_CONSERVATORY,LOUNGE], weapons=[CANDLESTICK]),
+
+				BILLIARD_ROOM: Room(name=BILLIARD_ROOM,connected=[HALL_BILLIARD,LIBRARY_BILLIARD,BILLIARD_BALLROOM,BILLIARD_DINING],weapons=[KNIFE]),
+
+				LIBRARY: Room(name=LIBRARY,connected=[STUDY_LIBRARY,LIBRARY_CONSERVATORY, LIBRARY_BILLIARD], weapons=[WRENCH]),
+
+				STUDY: Room(name = STUDY,connected = [KITCHEN,STUDY_LIBRARY, STUDY_HALL], weapons = [REVOLVER]),
+
+				HALL: Room( name = HALL,connected = [STUDY_HALL,HALL_BILLIARD, HALL_LOUNGE]),
+
+				LOUNGE: Room( name = LOUNGE, connected = [CONSERVATORY, HALL_LOUNGE,LOUNGE_DINING]),
+
+				DINING_ROOM: Room(name=DINING_ROOM, connected=[LOUNGE_DINING, BILLIARD_DINING, DINING_KITCHEN]),
+
+				STUDY_LIBRARY: Passage(name = STUDY_LIBRARY, connected=[STUDY, LIBRARY]),
+
+				STUDY_HALL: Passage(name=STUDY_HALL,connected=[ STUDY, HALL]),
+
+				HALL_BILLIARD: Passage(name=HALL_BILLIARD, connected=[HALL, BILLIARD_ROOM]),
+
+				HALL_LOUNGE: Passage( name = HALL_LOUNGE,connected =[HALL,LOUNGE]),
+
+				LOUNGE_DINING: Passage( name=LOUNGE_DINING,connected=[ LOUNGE, DINING_ROOM]),
+
+				LIBRARY_CONSERVATORY: Passage( name=LIBRARY_CONSERVATORY, connected=[ LIBRARY,CONSERVATORY]),
+
+				LIBRARY_BILLIARD: Passage(name=LIBRARY_BILLIARD, connected=[LIBRARY,BILLIARD_ROOM]),
+
+				BILLIARD_BALLROOM: Passage(name=BILLIARD_BALLROOM, connected=[BILLIARD_ROOM,BALLROOM]),
+
+				BILLIARD_DINING: Passage(name = BILLIARD_DINING, connected = [BILLIARD_ROOM,DINING_ROOM]),
+
+				DINING_KITCHEN: Passage(name=DINING_KITCHEN, connected=[DINING_ROOM,KITCHEN] ),
+
+				CONSERVATORY_BALLROOM: Passage(name = CONSERVATORY_BALLROOM, connected = [CONSERVATORY, BALLROOM]),
+
+				BALLROOM_KITCHEN: Passage(name = BALLROOM_KITCHEN, connected = [BALLROOM,KITCHEN]),
+
+				MUSTARD: HomeSpace(name = MUSTARD, connected=[LOUNGE_DINING], suspects=[MUSTARD]),
+
+				SCARLET: HomeSpace(name = SCARLET,connected=[HALL_LOUNGE], suspects=[SCARLET]),
+
+				PLUM: HomeSpace(name = PLUM, connected = [STUDY_LIBRARY], suspects=[PLUM]),
+
+				GREEN: HomeSpace(name= GREEN, connected = [CONSERVATORY_BALLROOM], suspects=[GREEN]),
+
+				WHITE: HomeSpace(name = WHITE, connected = [BALLROOM_KITCHEN], suspects=[WHITE]),
+
+				PEACOCK: HomeSpace(name = PEACOCK, connected = [LIBRARY_CONSERVATORY], suspects=[PEACOCK])
+				}
+
+
+
+	def format(self):
+
+		return {
+
+			"game": self.game,
+
+			"players": [player.format() for player in self.players],
+
+			"player_messages": self.player_messages,
+
+			'turn_list': [player.format() for player in self.turn_list],
+
+			"current_player": self.current_player.format(),
+
+			"turn_status": self.turn_status,
+
+			"current_suggestion": self.current_suggestion.format()
+				if self.current_suggestion else None,
+
+			"suggestion_response": self.suggestion_response.format()
+				if self.suggestion_response else None,
+
+			"case_file": [card.format() for card in self.case_file],
+
+			"game_active": self.game_active,
+
+			"game_winner": self.game_winner.format()
+				if self.game_winner else None,
+
+			"game_board": {
+
+				key:(self.game_board[key]).format()
+					for key in self.game_board
+			}
+		}
+
+# GameModelBuilder Class builds upon GameModel Dictionary and will be ultimately used/instantiated in our Server Class
+
+class GameModelBuilder(object):
+
+	def build_game(self, game_state):
+
+		game_state = game_state.copy()
+
+		game_state["players"] = self.build_players(game_state["players"])
+
+		game_state["turn_list"] = [
+
+			player for player in game_state["players"]
+			if player.name in [
+				turn_list_player["user"]
+				for turn_list_player in game_state["turn_list"]
+			]
 		]
 
+		if game_state["current_player"]:
+			game_state["current_player"] = [player for player in game_state["players"]
+				if player.name == game_state["current_player"]["name"]][0]
 
-class Weapon(object): 
+		game_board = dict()
 
+		for key in game_state["game_board"]:
 
-	WEAPON_LIST = [
-		
-		Rope, 
-		Knife, 
-		Revolver, 
-		Wrench, 
-		Knife, 
-		Lead Pipe, 
+			if key in ROOMS:
+				game_board[key] = Room(**game_state["game_board"][key])
+			if key in Passages:
+				game_board[key] = Passage(**game_state["game_board"][key])
+			if key in SUSPECTS:
+				game_board[key] = HomeSpace(**game_state["game_board"][key])
 
-	]
+		game_state["game_board"] = game_board
 
-
-class Room (Enum): 
-
-
-	ROOM_LIST = [
-
-		Ballroom, #Kitchen, Conservatory
-		Billiard Room, #Billard , Ballroom,  Hall, Conservatory
-		Conservatory, #Ballroom
-		Dining Room, #Kitchen, 
-		Hall, #Lounge
-		Kitchen, #Dining
-		Library, #Conservatory, Billiard
-		Lounge, #Hall , Dining 
-		Study, #Hall, Library, Balroom
-
-	]
+		return [GameModel(**game_state)]
 
 
-class Passage(): 
+	def build_game_cards(self, game_cards):
+
+		return [Card(**game_card_dict) for game_card_dict in game_cards]
 
 
+	def build_players(self, players):
 
+		for player_dict in players:
+			player_dict["card_hand"] = self.build_game_cards(
+				player_dict["card_hand"])
+			player_dict["card_seen"] = self.build_game_cards(
+				player_dict["card_seen"])
 
-class Player():
-
-	def __init__():
-
-
-
-class Card():
-
-	def __init__():
-
-
-
-class Suggestion():
-
-	def __init__():
-
-
-
-class Accusation():
-
-	def __init__():
-
-
-
-class Room():
-
-	def __init__(): 
-
-
-
-
-
-
-
-
+		return [Player(**player_dict) for player_dict in players]
 
 
--- Server.py
+++ Server.py
@@ -1,97 +1,354 @@
-#Reference: https://www.youtube.com/watch?v=BSGGAzsgFvk (Anguar and Python Socket IO)
-# pip install socketio
-
-from aiohttp import web
-import socketio
-
-
-socketio = socketio.AsyncServer()
-app = web.Application()
-socketio.attach(app)
-
-
-async def index(request):
-	return web.Response(text = "Hello World", content_type = 'text/html')
-
-
-@socketio.on('message')
-def print_message(sid, message)
-	print("Socket ID: ", sid)
-	print(message, broadcast = True)
-
-
-
-app.router.add_get('/', index)
-
-if __name__ == '__main__':
-	web.run(app)
-
+import random
+import Entity
 
 
 class Game(object):
 
 	def __init__(self):
 
-		self.player 
-		self.game_id
-		self.game_id
+		self.players = dict()
+		self.game_id = None
+		self.game = None
+
+	def add_player(self, name):
+
+		self.players[name] = Entity.Player(name)
+
+	def start_game(self):
+
+		game_players = [self.players[key] for key in self.players]
+		self.game = Entity.GameModel(self.game_id, game_players)
+
+		card_deck = Cards()
+		card_deck.shuffle_cards()
+		self.game.win_hand = card_deck.deal_winning_cards()
+
+		player_count = len(self.players)
+
+		hands = card_deck.deal_cards(player_count)
+		for x in range(player_count):
+			self.game.players[x].game_cards = hands[x]
 
 
-	def addPlayerToGame(self, name):
+	def end_game(self):
+
+		self.game = None
+		self.players = dict()
 
 
-	def startGame():
+	def make_move(self, player, suspect, room):
 
+		self.check_user(player)
+		self.check_turn(player)
+		self.check_turn_status(Entity.AWAITING_MOVE)
 
-	def endGame(self):
+		self.check_suspect(suspect)
+		self.check_player_suspect(suspect)
+		self.check_board(room)
+		self.check_board_available(room)
 
+		current_space = self.get_suspect_current_space(suspect)
+		new_space = self.game.game_board[room]
+		self.check_ifconnected(current_space, new_space)
 
-	def movePlayerPosition(self, player, suspect, room):
+		self.move_suspect(suspect, new_space)
 
-
-	def getSuggestion():
-
-
-
-	def sendSuggestion():
-
-
-
-	def getAccusation():
-
-
-
-	def endPlayerTurn():
-
-
-
-	def nextPlayerTurn();
-
-
-
-	def selectSuspect()
+		if isInstance(new_space, Entity.Room):
+			self.game.turn_status = Entity.AWAITING_SUGGESTION
+		else:
+			self.game.turn_status = Entity.AWAITING_ACCUSATION_OR_END_TURN
 
 
 
-class Cards(objecr):
+	def select_suspect(self, name, suspect):
+
+		self.check_suspect(suspect)
+		self.players[name].suspect = suspect
+
+
+	def make_suggestion(self, player, suspect, weapon, room):
+
+		self.check_user(player)
+		self.check_turn(player)
+		self.check_suspect(suspect)
+		self.check_weapon(weapon)
+		self.check_room(room)
+		self.check_suggestion_turn_status()
+		self.check_suggestion_room(room)
+
+		suggestion_room = self.game.game_board[room]
+		self.move_suspect(suspect, suggestion_room)
+		self.move_weapon(weapon, suggestion_room)
+
+		self.game.current_suggestion = Entity.Suggestion(suspect, weapon, room)
+
+
+	def respond_suggestion(self, player, card):
+
+		self.check_user(player)
+		self.check_suggestion_player(player)
+		self.check_card(card)
+		self.check_suggestion_card(card)
+
+		if card in Entity.WEAPONS:
+			item_type = Entity.WEAPON
+		elif card in Entity.ROOMS:
+			item_type = Entity.ROOM
+		else:
+			item_type = Entity.SUSPECT
+
+		cards = Entity.Card(card, item_type)
+
+		self.game.current_player.card_seen.append(cards)
+
+		self.game.turn_status = Entity.AWAITING_ACCUSATION_OR_END_TURN
+
+
+	def make_accusation(self, player, suspect, weapon, room):
+
+		self.check_user(player)
+		self.check_turn(player)
+		self.check_suspect(suspect)
+		self.check_weapon(weapon)
+		self.check_room(room)
+
+
+		accusation = [suspect, weapon, room]
+		winning_cards = [card.item for card in self.game.win_hand]
+		if set(accusation) == set(winning_cards):
+			self.game.game_active = False
+			self.game.game_winner = self.game.current_player
+
+		else:
+			lost_player = self.game.current_player
+			self.next_turn()
+			self.game.turn_list.remove(lost_player)
+
+
+	def end_turn(self, player):
+
+		self.check_user(player)
+		self.check_turn(player)
+		self.check_end_turn_status()
+		self.next_turn()
+
+
+
+
+	'''
+	Turn Queue Helpers
+	'''
+
+	def next_turn(self):
+
+		turn_index = self.game.turn_list.index(self.game.current_player)
+		if turn_index < (len(self.game.turn_list)-1):
+			turn_index += 1
+		else:
+			turn_index = 0
+		self.game.current_player = self.game.turn_list[turn_index]
+
+		self.game.turn_status = Entity.AWAITING_MOVE
+
+		current_space = self.get_suspect_current_space(self.game.current_player.suspect)
+		move_available = False
+
+		for space in current_space.connected:
+			if self.game.game_board[space].available():
+				move_available = True
+
+		if not move_available:
+			self.game.turn_status = Entity.AWAITING_ACCUSATION_OR_END_TURN
+
+
+
+	def next_player_index(self):
+		turn_index = self.game.turn_lsit.index(self.game.current_player)
+		if turn_index < (len(self.game.turn_list)-1):
+			turn_index += 1
+		else:
+			turn_index = 0
+
+		return turn_index
+
+
+	def check_turn_status(self, status):
+		if self.game.turn_status != status:
+			raise SyntaxError
+
+	def check_turn(self, player):
+		if player != self.game.current_player.player:
+			raise SyntaxError
+
+
+	def check_end_turn_status(self):
+		if self.game.turn_status != Entity.AWAITING_ACCUSATION_OR_END_TURN:
+			raise SyntaxError
+
+
+
+
+	'''
+	Move Object Helpers	
+	'''
+
+	def check_room(self, room):
+		if room not in Entity.ROOMS:
+			raise
+
+	def check_board_available(self, room):
+		if not self.game.game_board[room].isAvailable():
+			raise SyntaxError
+
+	def check_board(self, room):
+		if not self.game.game_board[room]:
+			raise SyntaxError
+
+	def check_ifconnected(self, current_space, new_space):
+		if new_space.name not in current_space.connected:
+			raise SyntaxError
+
+
+
+
+	'''
+	Suggestion Helpers	
+	'''
+
+	def move_suspect(self, suspect, suggestion_room):
+		current_space = self.get_suspect_current_space(suspect)
+		current_space.suspects.remove(suspect)
+		suggestion_room.suspects.append(suspect)
+
+
+	def move_weapon(self, weapon, room):
+
+		current_room = self.get_weapon_current_space(weapon)
+		current_room.weapons.remove(weapon)
+		room.weapons.appednd(weapon)
+
+
+	def get_weapon_current_space(self, weapon):
+
+		game_board = self.game.game_board
+		rooms = [room for room in [game_board[space]
+				for space in game_board
+				if isInstance(game_board[space], Entity.Room)]]
+
+		for room in rooms:
+			if weapon in room.weapons:
+				return room
+
+	def get_suspect_current_space(self, suspect):
+		for space in self.game.game_board:
+			if suspect in self.game.game_board[space].suspects:
+				return self.game.game_board[space]
+
+
+	def check_weapon(self, weapon):
+
+		if weapon not in Entity.WEAPONS:
+			raise SyntaxError
+
+
+	def check_suspect(self, suspect):
+		if suspect not in Entity.SUSPECTS:
+			raise SyntaxError
+
+
+	def check_player_suspect(self, suspect):
+		if suspect != self.game.current_player.suspect:
+			raise SyntaxError
+
+	def check_user(self, name):
+
+		for player in self.game.players:
+			if name == player.name:
+				return
+		raise SyntaxError
+
+
+	def check_card(self, card):
+
+		valid_items = Entity.SUSPECTS
+		valid_items += Entity.WEAPONS
+		valid_items += Entity.ROOMS
+
+		if card not in valid_items:
+			raise SyntaxError
+
+	def check_suggestion_card(self, card):
+		cards = self.game.suggestion_response.game_cards
+		player_items = [ card.item for card in cards]
+
+		if card not in player_items:
+			raise SyntaxError
+
+
+
+	def check_suggestion_room(self, room):
+		suspect = self.game.current_player.suspect
+		if suspect not in self.game.game_board[room].suspects:
+			raise SyntaxError
+
+
+	def check_suggestion_player(self, player):
+		if player != self.game.suggestion_response.player:
+			raise SyntaxError
+
+	def check_suggestion_turn_status(self):
+		if self.game.turn_status != Entity.AWAITING_SUGGESTION:
+			raise SyntaxError
+
+
+
+
+
+
+
+
+class Cards(object):
 
 
 	def __init__(self):
 
-		#initialize suspects
-		#initialize rooms
-		#initialize weapons
-		#initialize 
+		self.suspects = [
+			Entity.Card(item = item, item_type = Entity.SUSPECT)
+			for item in Entity.SUSPECTS]
 
+		self.weapons = [
+			Entity.Card(item = item, item_type = Entity.WEAPON)
+			for item in Entity.WEAPONS]
 
-	def shuffleCards(self):
+		self.rooms = [
+			Entity.Card(item = item, item_type = Entity.ROOM)
+			for item in Entity.ROOMS]
 
+		self.game_cards = self.suspects + self.weapons + self.rooms
 
-	def dealCards(self):
+		self.winning_cards = list()
 
+	def shuffle_cards(self):
+		random.shuffle(self.game_cards)
 
-	def winningCards(self):
+	def deal_cards(self, hands):
+		return [self.game_cards[x::hands] for x in range(hands)]
 
+	def deal_winning_cards(self):
 
+		win_suspect = self.suspects[random.choice(range(len(self.suspects)))]
+
+		win_weapon = self.weapons[random.choice(range(len(self.weapons)))]
+
+		win_room = self.rooms[random.choice(range(len(self.rooms)))]
+
+		self.winning_cards = [win_suspect, win_weapon, win_room]
+
+		self.game_cards = [
+			game_card for game_card in self.game_cards
+			if game_card not in self.winning_cards
+		]
+
+		return self.winning_cards
 
 

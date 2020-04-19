8dbba20e8a01bca5b2ad17506cdacc62c6ec2334
T <32947002+cheersmate@users.noreply.github.com>
2020-04-19 11:54:08 -0700

    Update API.py
    changed select_suspect to select_character

--- API.py
+++ API.py
@@ -8,9 +8,16 @@ class Resource:
     def main():
 
         game.add_player("1")
-        game.select_suspect("1", "Miss Scarlet")
+        game.select_character("1", "Miss Scarlet")
         game.add_player("2")
-        game.select_suspect("2", "Professor Plum")
+        game.select_character("2", "Mrs White")
+
+        game.start_game()
+        game.make_move("1", "Miss Scarlet", "hall-lounge Passage")
+        game.end_turn("1")
+
+
+    '''
         game.add_player("3")
         game.select_suspect("3", "Mrs Peacock")
         game.add_player("4")
@@ -19,14 +26,11 @@ class Resource:
         game.select_suspect("5", "Mr Green")
         game.add_player("6")
         game.select_suspect("6", "Mrs White")
+'''
 
 
-        game.start_game()
-
-        game.make_move("1", "Miss Scarlet", "hall-lounge Passage")
-        game.end_turn("1")
-
-  
+        #game.make_move("Tolly", "Professor Plum", "study-library Passage")
+        #game.end_turn("Tolly")
 
 
 

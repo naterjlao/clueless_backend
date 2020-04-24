f53b4e833bdcbfcc836d4f47cf34ed00bb55f319
cheersmate <ptpowell888@gmail.com>
2020-04-23 19:25:33 -0700

    Updated API run examples and Errors

--- API.py
+++ API.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python3
 from Server import Game
 
+from Server import Game
+
 game = Game()
 
 class Resource:
 
     def main():
 
+
         game.add_player("1")
         game.select_character("1", "Miss Scarlet")
         game.add_player("2")
         game.select_character("2", "Mrs White")
 
+        '''
+        game.select_character("3", "Colonel Mustard")
+        game.add_player("4")
+        game.select_character("4", "Mrs Peacock")
+  
+        '''
+
         game.start_game()
+
+        game.check_move_options(game.get_suspect_current_space("Miss Scarlet").name)
         game.make_move("1", "Miss Scarlet", "hall-lounge Passage")
         game.end_turn("1")
+
+        game.check_move_options(game.get_suspect_current_space("Mrs White").name)
+        game.make_move("2", "Mrs White", "ballroom-kitchen Passage")
+        game.end_turn("2")
+
+        game.check_move_options(game.get_suspect_current_space("Miss Scarlet").name)
+        game.make_move("1", "Miss Scarlet", "Hall")
+        game.make_suggestion("1", "Colonel Mustard", "Wrench", "Hall")
+        game.end_turn("1")
+
+        game.make_move("2", "Mrs White", "Ballroom")
+        game.make_suggestion("2", "Miss Scarlet", "Wrench", "Ballroom")
+        game.end_turn("2")
+
+        #game.end_turn("2")
+
+        #game.make_suggestion("Miss Scarlet", "Colonel Mustard", "Wrench", "Lounge")
+        #game.end_turn("1")
 
 
     '''
@@ -26,13 +56,18 @@ class Resource:
         game.select_suspect("5", "Mr Green")
         game.add_player("6")
         game.select_suspect("6", "Mrs White")
+        
+       # game.make_move("3", "Colonel Mustard", "lounge-dining Passage")
+       # game.end_turn("3")
+       
+       
+        #game.make_move("Tolly", "Professor Plum", "study-library Passage")
+        #game.end_turn("Tolly")
+        
 '''
 
 
-        #game.make_move("Tolly", "Professor Plum", "study-library Passage")
-        #game.end_turn("Tolly")
 
 
 
     main()
-
--- ErrorServer.py
+++ ErrorServer.py
@@ -1,25 +1,23 @@
 
+
+
 class GameException(Exception):
     pass
 
 
-class InvalidMove(GameException):
+class InvalidTurnList(GameException):
 
     def __init__(self):
-        msg = "Try a different Move"
-        super(InvalidMove, self).__init__(msg)
+        msg = "It's not your turn mate"
+        super(InvalidTurnList, self).__init__(msg)
 
 
 class InvalidTurnStatus(GameException):
 
     def __init__(self):
-        msg = "It's not your turn mate"
+        msg = "Wrong move"
         super(InvalidTurnStatus, self).__init__(msg)
 
-class InvalidEndTurn(GameException):
-    def __init__(self):
-        msg = "You can't end your turn yet, try another action"
-        super(InvalidEndTurn, self).__init__(msg)
 
 class InvalidSuspect(GameException):
 
@@ -27,8 +25,29 @@ class InvalidSuspect(GameException):
         msg = "You don't own this suspect"
         super(InvalidSuspect, self).__init__(msg)
 
+#    def _validate_current_player_owns_suspect(self, suspect):
+
+# not needed
+class InvalidMove(GameException):
+    def __init__(self):
+        msg = "You can't move there"
+        super(InvalidMove, self).__init__(msg)
+
+
 class InvalidSuggestion(GameException):
 
     def __init__(self):
         msg = "Must be in same room"
         super(InvalidSuggestion, self).__init__(msg)
+
+
+class InvalidCard(GameException):
+    def __init__(self):
+        msg = "You do not own the card"
+        super(InvalidCard, self).__init__(msg)
+
+
+class InvalidEndTurn(GameException):
+    def __init__(self):
+        msg = "You can't end your turn yet, try another action"
+        super(InvalidEndTurn, self).__init__(msg)
